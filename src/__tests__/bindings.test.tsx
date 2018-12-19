1273ed8372ed027c660054ba3ba403a6db7af2e9
James Wright <james@jamesswright.co.uk>
2018-12-19 18:18:24 +0000

    Adds tests for mapState/DispatchToProps

--- src/__tests__/bindings.test.tsx
+++ src/__tests__/bindings.test.tsx
@@ -1,20 +1,30 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { Provider, connect, Reducer } from '../bindings';
 import { Action } from '../actions/actions';
 import { State, defaultState } from '../state';
+import { DispatchProps } from '../components/MessageForm';
 
 interface OwnProps {
   baz: string;
 }
 
+interface StateProps {
+  hasMessages: boolean;
+  hasBaz: boolean;
+}
+
+interface DispatchProps {
+  setBaz(): void;
+}
+
 const MyComponent: React.FC<OwnProps> = ({ baz }) =>
-  <p>{baz}</p>;
+  <div>{baz}</div>;
 
 // TODO: explain why injecting useReducer
 describe('bindings', () => {
   describe('Provider with connect', () => {
-    it('should pass any other props to the connected component', () => {
+    it('should pass any outer props to the connected component', () => {
       const reducer = (s: State, a: Action) => s;
       const dispatch = (action: Action) => undefined;
       const useReducer = (r: Reducer, s: State) => [defaultState, dispatch] as [State, React.Dispatch<Action>];
@@ -30,10 +40,56 @@ describe('bindings', () => {
         </Provider>
       );
 
-      const renderedRoot = shallow(<Root />);
+      const renderedRoot = mount(<Root />);
       const renderedChild = renderedRoot.find(ConnectedComponent);
 
       expect(renderedChild.prop('baz')).toBe('qux');
+    });
+
+    it('should pass the props returned by mapStateToProps and mapDispatchToProps to the wrapped components', () => {
+      const state = {
+        ...defaultState,
+        messages: [
+          'foo!',
+        ],
+      };
+
+      const setBaz = () => undefined;
+      const reducer = (s: State, a: Action) => s;
+      const dispatch = (action: Action) => undefined;
+      const useReducer = (r: Reducer, s: State) => [state, dispatch] as [State, React.Dispatch<Action>];
+
+      const mapStateToProps = (state: State, ownProps: OwnProps): StateProps => ({
+        hasMessages: !!state.messages.length,
+        hasBaz: !!ownProps.baz,
+      });
+
+      const mapDispatchToProps = (dispatch: React.Dispatch<Action>, ownProps: OwnProps): DispatchProps => ({
+        setBaz,
+      });
+
+      const ConnectedComponent = connect<StateProps, DispatchProps, OwnProps>(
+        mapStateToProps,
+        mapDispatchToProps,
+      )(MyComponent);
+
+      const Root = () => (
+        <Provider
+          defaultState={defaultState}
+          reducer={reducer}
+          useReducer={useReducer}
+        >
+          <ConnectedComponent baz="qux" />
+        </Provider>
+      );
+
+      const renderedRoot = mount(<Root />);
+      const renderedChild = renderedRoot.find(MyComponent);
+
+      expect(renderedChild.prop('baz')).toBe('qux');
+      expect(renderedChild.prop('hasMessages')).toBe(true);
+      expect(renderedChild.prop('hasBaz')).toBe(true);
+      expect(renderedChild.prop('setBaz')).toBe(setBaz);
     });
   });
 });
